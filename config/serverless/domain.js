6a5872b079333c6a2000f4a1fef2a0ce8049e50b
Luciano Bertenasco <lbertenasco@gmail.com>
2020-09-14 15:19:43 -0300

    Add sls domain.js config

---
+++ config/serverless/domain.js
@@ -0,0 +1,29 @@
+const _ = require('lodash');
+
+const DEFAULT = {
+  domainName: 'api.yearn.tools',
+  certificateName: 'yearn.tools',
+  stage: '${self:custom.currentStage}',
+  createRoute53Record: true,
+  basePath: '${self:custom.basePath}',
+  endpointType: 'regional',
+  apiType: 'http',
+  securityPolicy: 'tls_1_2',
+  autoDomain: true,
+  // allowPathMatching: true, // enable only once when migrating from rest to http api migration
+};
+
+module.exports.prod = () => DEFAULT;
+
+module.exports.staging = () => {
+  return _.merge({}, DEFAULT, {
+    domainName: `staging-${DEFAULT.domainName}`,
+  });
+};
+
+module.exports.dev = () => {
+  return _.merge({}, DEFAULT, {
+    domainName: `dev-${DEFAULT.domainName}`,
+  });
+};
+
