6ea785d92584f3057ee4e812512cc0476d29e248
x48 <x48114@protonmail.com>
2020-09-19 11:25:05 -0500

    Increase token and vault icon size in vaults API

--- services/vaults/save-vaults/handler.js
+++ services/vaults/save-vaults/handler.js
@@ -27,7 +27,7 @@ const vaultAliases = {
 
 const vaultIcons = {
 	"yDAI+yUSDC+yUSDT+yTUSD":
-		"https://assets.coingecko.com/coins/images/12210/small/yUSD.png?1600166557",
+		"https://assets.coingecko.com/coins/images/12210/large/yUSD.png?1600166557",
 };
 
 const fetchContractMetadata = async (address) => {
@@ -115,7 +115,7 @@ module.exports.handler = async (event) => {
 		const symbolAlias = symbolAliases[vaultSymbol] || `y${tokenSymbolAlias}`;
 		const vaultAlias =
 			vaultAliases[vaultAddress] || `${tokenSymbolAlias} Vault`;
-		const tokenIcon = tokenInfo.image.small;
+		const tokenIcon = tokenInfo.image.large;
 		const vaultIcon = vaultIcons[tokenSymbol];
 		const vault = {
 			address: vaultAddress,
