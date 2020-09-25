39965268c4fef18cfc1fe6cf0db6e3553c285be9
x48 <x48114@protonmail.com>
2020-09-25 01:39:32 -0500

    Add API for fetching vault addresses

---
+++ services/vaults/addresses/handler.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const _ = require("lodash");
+const { getVaults } = require("../handler");
+
+module.exports.handler = async (event) => {
+  const vaults = await getVaults();
+  const addresses = _.map(vaults, "address");
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(addresses),
+  };
+};
