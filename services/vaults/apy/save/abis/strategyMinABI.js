8b48b02d02695bad0b8d1d69e9ba315f7567b1c1
Graham <grahamu@gmail.com>
2020-10-21 11:35:29 +0100

    Add CRV reward boost data to /vaults/apy endpoint.

---
+++ services/vaults/apy/save/abis/strategyMinABI.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    constant: true,
+    inputs: [],
+    name: "gauge",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "voter",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
