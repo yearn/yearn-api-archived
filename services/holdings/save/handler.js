413614a5bb9637d00e34e782b2deb59b00373801
Miguel Cabeza <miguel567@gmail.com>
2020-11-28 17:59:15 +0100

    starting

---
+++ services/holdings/save/handler.js
@@ -0,0 +1,111 @@
+/* 
+
+Holdings calculated here are for Vaults, Strategies and the Earn yTokens
+This is part of the TVL calculation defined here: https://hackmd.io/@dudesahn/BkxKfTzqw
+
+ */
+
+'use strict';
+
+require('dotenv').config();
+const dynamodb = require('../../../../utils/dynamoDb');
+const Web3 = require("web3");
+const moment = require("moment");
+const delay = require("delay");
+const _ = require("lodash");
+const vaults = require("./vaults");
+const EthDater = require("./ethereum-block-by-date.js.js");
+const { delayTime } = require("./config");
+
+const { getHoldings } = require('./getHoldings');
+
+const db = dynamodb.doc;
+const archiveNodeUrl = process.env.ARCHIVENODE_ENDPOINT;
+const infuraUrl = process.env.WEB3_ENDPOINT;
+const archiveNodeWeb3 = new Web3(archiveNodeUrl);
+const infuraWeb3 = new Web3(infuraUrl);
+const blocks = new EthDater(archiveNodeWeb3, delayTime);
+const axios = require('axios');
+
+
+
+const saveVault = async (data) => {
+  const params = {
+    TableName: "holdings",
+    Item: data,
+  };
+  await db
+    .put(params)
+    .promise()
+    .catch((err) => console.log("err", err));
+  console.log(`Saved ${data.name}`);
+};
+
+
+
+
+const readVault = async (vault) => {
+  const {
+    name,
+    symbol,
+    description,
+    vaultSymbol,
+    vaultContractABI: abi,
+    vaultContractAddress: address,
+    erc20address: tokenAddress,
+    price_id: price_id
+  } = vault;
+  console.log(`Reading vault ${vault.name}`);
+  if (!abi || !address) {
+    console.log(`Vault ABI not found: ${name}`);
+    return null;
+  }
+  try{
+    const holdings= await getHoldings(vault);
+    const priceFeed = await axios.get('https://api.coingecko.com/api/v3/coins/' + vault.price_id);
+    console.log("Vault: ", name);
+    const data = {
+      address,
+      name,
+      symbol,
+      price_id,
+      price_usd: priceFeed.data.market_data.current_price.usd,
+      timestamp: Date.now(),
+      holdings    
+    };
+  await saveVault(data);
+    console.log(data);
+    return data;
+  } catch(e) {
+    console.log("error", e);
+    return e
+  }
+ 
+  
+};
+
+module.exports.handler = async (context) => {
+  let vaultsTVL = 0;
+
+  const vaultsWithHoldings = [];
+  for (const vault of vaults) {
+    const vaultWithHoldings = await readVault(vault);
+    /* vaultsTVL += (vaultWithHoldings.holdings.vaultHoldings + vaultWithHoldings.holdings.strategyHoldings) * vaultWithHoldings.price_usd; */
+    /* console.log(vaultsTVL); */
+    if (vaultWithHoldings !== null) {
+      vaultsWithHoldings.push(vaultWithHoldings);
+    }
+    
+    await delay(delayTime);
+  }
+  vaultsWithHoldings.push({TotalvaultsTVL: vaultsTVL});
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(vaultsWithHoldings),
+  };
+  return response;
+};
---
+++ services/tvl/save/config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const vaultContractABI = require('../../../../abi/vaultV1');
+const vaultContractV2ABI = require('../../../../abi/vaultV2');
+const vaultContractV3ABI = require('../../../../abi/vaultV3');
+const vaultContractV4ABI = require('../../../../abi/vaultV4');
+const vaultContractV5ABI = require('../../../../abi/vaultV5');
+
+module.exports = {
+  delayTime: 1000,
+  vaultContractABI,
+  vaultContractV2ABI,
+  vaultContractV3ABI,
+  vaultContractV4ABI,
+  vaultContractV5ABI,
+};
