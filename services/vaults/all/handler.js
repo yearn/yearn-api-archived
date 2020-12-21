98324a65f05230a2c44f62491edc866c6268add9
nymmrx <nymmrx@protonmail.com>
2020-12-21 23:31:21 +0100

    fix(vaults): use batch calls for vaults/all endpoint

--- services/vaults/all/handler.js
+++ services/vaults/all/handler.js
@@ -2,28 +2,54 @@
 
 const _ = require('lodash');
 
+const Web3BatchCall = require('web3-batch-call');
+
 const handler = require('../../../lib/handler');
 
-const { getVaults } = require('../handler');
-const { getVaultsV2 } = require('../v2/handler');
-
-const V2_KEYMAP = {
-  vault: 'address',
-  vaultName: 'name',
-  vaultSymbol: 'symbol',
-  vaultDecimals: 'decimals',
-};
+const v1 = require('../lib/v1');
+const v2 = require('../lib/v2');
 
 module.exports.handler = handler(async () => {
-  const v1 = _(await getVaults())
-    .map((val) => _.pick(val, ['address', 'name', 'symbol', 'decimals']))
-    .map((val) => _.assign({}, val, { type: 'v1' }))
-    .value();
+  const v1Addresses = await v1.fetchAddresses();
+  const v2Addresses = await v2.fetchAddresses();
 
-  const v2 = _(await getVaultsV2())
-    .map((val) => _.mapKeys(val, (__, k) => V2_KEYMAP[k]))
-    .map((val) => _.pick(val, ['address', 'name', 'symbol', 'decimals']))
-    .map((val) => _.assign({}, val, { type: 'v2' }))
-    .value();
-  return _.merge(v1, v2);
+  // TODO: ugly
+  const provider = process.env.WEB3_ENDPOINT;
+  const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+
+  const batchCall = new Web3BatchCall({
+    provider,
+    etherscan: {
+      apiKey: etherscanApiKey,
+      delay: 300,
+    },
+  });
+
+  const contracts = [
+    {
+      namespace: 'v1',
+      addresses: v1Addresses,
+      abi: v1.abi(),
+      readMethods: [{ name: 'name' }, { name: 'symbol' }, { name: 'decimals' }],
+    },
+    {
+      namespace: 'v2',
+      addresses: v2Addresses,
+      abi: v2.abi(),
+      readMethods: [{ name: 'name' }, { name: 'symbol' }, { name: 'decimals' }],
+    },
+  ];
+
+  const res = await batchCall.execute(contracts);
+  const data = _(res).map(
+    ({ address, namespace: type, name, symbol, decimals }) => ({
+      address,
+      type,
+      name: name[0].value,
+      symbol: symbol[0].value,
+      decimals: decimals[0].value,
+    }),
+  );
+
+  return data;
 });
